78c32c2606bf595291ccae95b953caed8d5f68bd
Gilbert Torchon <gilbert@noukod.com>
2018-11-13 21:30:16 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -35,10 +35,7 @@ end
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
 "SELECT projects.category,SUM(pledges.amount)
 FROM projects
-WHERE projects.category='music'
-INNER JOIN pledges
-ON project.id=pledges.project_id
-GROUP BY projects.name
+
 "
 
 end
